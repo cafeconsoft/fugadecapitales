a14932e1c6d1753a78b82d80430dc1454482caf5
alexander171294 <2080955+alexander171294@users.noreply.github.com>
2019-07-20 14:30:49 -0300

    component declarations.

--- capitalflight-frontend/src/app/app-routing.module.ts
+++ capitalflight-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { ServerSelectComponent } from './pages/server-select/server-select.component';
+import { PoliticsComponent } from './pages/politics/politics.component';
+import { EconomyComponent } from './pages/economy/economy.component';
+import { MarketComponent } from './pages/market/market.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'server', component: ServerSelectComponent },
+  { path: 'politics', component: PoliticsComponent },
+  { path: 'economy', component: EconomyComponent },
+  { path: 'market', component: MarketComponent }
 ];
 
 @NgModule({
--- capitalflight-frontend/src/app/app.module.ts
+++ capitalflight-frontend/src/app/app.module.ts
@@ -4,11 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ServerSelectComponent } from './pages/server-select/server-select.component';
+import { EconomyComponent } from './pages/economy/economy.component';
+import { PoliticsComponent } from './pages/politics/politics.component';
+import { MarketComponent } from './pages/market/market.component';
+import { ServerCardComponent } from './components/server-card/server-card.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { ChatButtonComponent } from './components/chat-button/chat-button.component';
+import { InfoMenuComponent } from './components/info-menu/info-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ServerSelectComponent,
+    EconomyComponent,
+    PoliticsComponent,
+    MarketComponent,
+    ServerCardComponent,
+    MainMenuComponent,
+    ChatButtonComponent,
+    InfoMenuComponent
   ],
   imports: [
     BrowserModule,
