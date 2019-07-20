cd3a9c31d133f18ec2c469137402841e2e8ae0c6
alexander171294 <2080955+alexander171294@users.noreply.github.com>
2019-07-20 15:38:38 -0300

    economy slider.

--- capitalflight-frontend/src/app/pages/economy/economy.component.ts
+++ capitalflight-frontend/src/app/pages/economy/economy.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-economy',
   templateUrl: './economy.component.html',
-  styleUrls: ['./economy.component.scss']
+  styleUrls: [
+    './economy.component.scss',
+    '../../utils/css/generals.scss'
+  ]
 })
 export class EconomyComponent implements OnInit {
 
-  constructor() { }
+  sliderStyle: string;
+
+  constructor() {
+    this.sliderStyle = 'left: -100%';
+  }
 
   ngOnInit() {
+  }
+
+  industriesFD(evt) {
+    this.sliderStyle = 'left: -100%';
+  }
+
+  business(evt) {
+    this.sliderStyle = 'left: -200%';
+  }
+
+  detail(evt) {
+    this.sliderStyle = 'left: 0%';
+  }
+
+  industriesFB(evt) {
+    this.sliderStyle = 'left: -100%';
   }
 
 }
