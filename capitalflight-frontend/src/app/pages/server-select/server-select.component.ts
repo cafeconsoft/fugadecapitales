c56f998b4b06a401fa1763de964b390d4f38d012
alexander171294 <2080955+alexander171294@users.noreply.github.com>
2019-07-20 14:29:42 -0300

    server select page

---
+++ capitalflight-frontend/src/app/pages/server-select/server-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-select',
+  templateUrl: './server-select.component.html',
+  styleUrls: ['./server-select.component.scss']
+})
+export class ServerSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
