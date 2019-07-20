525f5e51728f3d157cd5810bab60cd0e613ed91e
alexander171294 <2080955+alexander171294@users.noreply.github.com>
2019-07-20 17:14:28 -0300

    prevent show menu in select server and login

--- capitalflight-frontend/src/app/app.component.ts
+++ capitalflight-frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'capitalflight-frontend';
+  showMenues: boolean;
+
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        this.showMenues = event.url !== '/' && event.url !== '/server';
+      }
+    });
+  }
+
+  ngOnInit(): void { }
+
 }
